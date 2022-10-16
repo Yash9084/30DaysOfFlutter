e280cbdeb4b6f37ce7345463533942d35def4bba
Yash Pratap Rana <yr85088@gmai.com>
2022-10-16 17:57:41 +0530

    Day 9 completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -13,7 +13,9 @@ class HomePage extends StatelessWidget {
         title: const Text("Catalog App"),
       ),
       body: Center(
+        child: Container(
         child: Text("Welcome to $days days of flutter by $name"),
+      ),
       ),
       drawer: const MyDrawer(),
     );
